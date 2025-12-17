00fc46f07b76160d0b6985af6b5524f9b401aac7
ColetivoSend Deploy <deploy@coletivosend.com>
2025-12-17 15:38:37 -0300

    Fix: Disable standalone output mode temporarily

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone', // Disabled primarily to debug 404s
   images: {
     remotePatterns: [
       {
